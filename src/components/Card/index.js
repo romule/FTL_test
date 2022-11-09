39b7d3afa09a74258fdf0c80677427f2c673b733
RiO <43984905+romule@users.noreply.github.com>
2022-11-09 13:39:17 +0200

    add placeholder to img

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,21 +1,34 @@
 import "../../assets/styles/card.css";
-import { forwardRef } from "react";
+import Placeholder from "../../assets/img/cat_placeholder.png";
+import { forwardRef, useState } from "react";
 
 const CardOfCats = forwardRef((props, ref) => {
+  const [hasError, setError] = useState(false);
+
+  function replaceIMG(error) {
+    setError(true);
+    return (error.target.src = Placeholder);
+  }
+
   return (
     <div
       ref={ref}
       className={props.cat.available ? "card-box" : "disabled card-box"}
     >
+      <div className="card-box_price">
+        {props.cat.available ? (
+          <span>{"🔖 For sale $" + props.cat.price}</span>
+        ) : (
+          <span>{"❌ Unavailable"}</span>
+        )}
+      </div>
       <div className="card-box_img">
-        <div className="card-box_price">
-          {props.cat.available ? (
-            <span>{"🔖 For sale $" + props.cat.price}</span>
-          ) : (
-            <span>{"❌ Unavailable"}</span>
-          )}
-        </div>
-        <img src={props.cat.image_url} alt="photo_of_cat" />
+        <img
+          className={hasError ? "img-error" : ""}
+          src={props.cat.image_url}
+          alt="photo_of_cat"
+          onError={replaceIMG}
+        />
       </div>
       <div className="card-box_info">
         <p>{"Name: " + props.cat.name}</p>
